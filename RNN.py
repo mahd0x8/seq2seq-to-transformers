c358e5538a6687dbc05fcbf276c0916cc1464ef1
Mahd Bin Naeem <mahdbinnaeem@gmail.com>
2025-05-10 23:23:29 +0500

    Train RNN to overfit on single sample for sanity check

---
+++ RNN.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import SimpleRNN, Dense, Dropout, TimeDistributed, Input
+
+def RNN(X_train):
+    # Build the RNN model
+    """model = Sequential([
+        Input(shape=(X_train.shape[1], 1)),
+        
+        # First RNN layer (return sequences to stack another RNN)
+        SimpleRNN(128, return_sequences=True),
+        Dropout(0.2),
+        
+        # Second RNN layer
+        SimpleRNN(64, return_sequences=True),
+        Dropout(0.2),
+        
+        # TimeDistributed Dense layer to output 1 value per timestep
+        TimeDistributed(Dense(1))
+    ])"""
+    
+    # Define model
+    model = Sequential([
+        Input(shape=(8, 1)),
+        SimpleRNN(64, return_sequences=True),
+        TimeDistributed(Dense(1))
+    ])
+
+    # Compile
+    model.compile(optimizer='adam', loss='mse')
+    
+    return model
